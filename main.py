e6f05105608e7c0b4f2002f6dc2bea8d5144cd3b
Raghav <Raghav0482@gmail.com>
2021-06-10 20:54:05 +0530

    final commit

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import speech_recognition as sr
 from tkinter.ttk import *
 from tkinter import messagebox
 from tkinter.filedialog import *
-
+import os
+from gtts import gTTS
 
 Window= Tk()
 #add title
@@ -12,12 +13,13 @@ Window.title("Language Processor-2")
 #add dimensions 
 # Window.geometry("800x400")
 
+font_tup = ("Comic Sans MS", 15)
 
 #heading 
 heading1 = Label(Window, text = "Voice Notepad",font = "bold, 30 ")
 heading1.grid(row=0,column=1,padx=20,pady=20)
 #Text label
-Output_text = Text(Window, height= 4, width = 40)
+Output_text = Text(Window, height= 4, width = 40,  font= font_tup)
 Output_text.grid(row=1,column=1,pady = 20,padx=20)
 
 #function     
@@ -47,13 +49,33 @@ def save():
   
 
 # our convert to text function using command = To_Text 
-startVoive_btn = Button(Window, text = 'Click on me!..\nTo start recording',
+startVoive_btn = Button(Window, text = 'Speech-to-text',
                    font = 'bold, 15', command = To_Text,width=20)
 startVoive_btn.grid(row=1,column=0,pady = 20,padx=20)
 
 # our SAVE function using command =save
-save_button = Button(Window, text='Save the Text',width=20,height=4,command=save)
+save_button = Button(Window, text='Save the Text',font = 'bold, 15',width=20,command=save)
 save_button.grid(row = 1, column = 2,pady = 10,columnspan=3)
+
+
+
+
+
+
+### Text-to-speech
+entry = Text(Window, width = 40, height=4, font=font_tup) 
+entry.grid(row=2,column=1,pady = 20,padx=20)
+
+
+def play():
+    language = 'en'
+    output_audio = gTTS(text = entry.get() , lang= language)
+    output_audio.save("convert.mp3")
+    os.system("convert.mp3")
+
+btn_txt_sc= Button(Window, text = "Text-to-speech",font = 'bold, 15',width=20, command=play)
+btn_txt_sc.grid(row=2,column=0,padx=10)
+
 
 
 # start the gui 
